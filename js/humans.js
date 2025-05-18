93ffbaaa0ffd764d99adc6f930a307e0b183172e
Ayyubxon Munavvarjonov <117116942+M1rzohidovich@users.noreply.github.com>
2025-05-18 21:18:14 +0100

    Update humans.js

--- js/humans.js
+++ js/humans.js
@@ -13,7 +13,7 @@ function renderHumans(data){
     cardList.innerHTML += 
         `
         <li class="humans-card__item">
-            <img class="humans-card__img" src="https://ayyubxon.pythonanywhere.com/${item.img ? item.img : './img/search.png '}" alt="Чистый дом">
+            <img class="humans-card__img" src="https://anonimuser.pythonanywhere.com/${item.img ? item.img : './img/search.png '}" alt="Чистый дом">
             <span class="humans-rating"><i class="fas fa-star"></i><span class="rating-num">${item.Rating}</span></span>
             <h3 class="humans-name">${item.Fullname}</h3>
             <p class="humans-descreption">The task: ${item.the_task}</p>
